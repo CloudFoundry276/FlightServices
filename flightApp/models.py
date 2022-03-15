7a15ca548d7e25ef7cac893c5f8bf68b7975f6e2
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-15 23:53:24 +0530

    created model classes

--- flightApp/models.py
+++ flightApp/models.py
@@ -1,3 +1,22 @@
+from re import M
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    flight_number = models.CharField(max_length=12)
+    operating_airlines = models.CharField(max_length=50)
+    departure_city = models.CharField(max_length=30)
+    arrival_city = models.CharField(max_length=30)
+    departure_date = models.DateField()
+    estimated_departure_time = models.TimeField()
+
+class Passenger(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    middle_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+
+class Reservation(models.Model):
+    flight = models.OneToOneField(Flight, on_delete=models.CASCADE)
+    passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE)
